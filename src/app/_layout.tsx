693f56e1dd688556ab96a4872fce4f66b24ac431
Alex Muiruri <content@alexmuiruri.com>
2026-01-15 11:13:16 +0300

    Created a simple webview

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,33 +1,23 @@
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native"
+import { useColorScheme } from "@/src/hooks/use-color-scheme"
 import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import "react-native-reanimated"
 
-import { useColorScheme } from "@/src/hooks/use-color-scheme"
+export const unstable_settings = { anchor: "index" }
 
-export const unstable_settings = {
-    anchor: "(tabs)",
-}
-
-export default function RootLayout() {
+const RootLayout = () => {
     const colorScheme = useColorScheme()
 
     return (
-        <ThemeProvider
-            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-            <Stack>
-                <Stack.Screen name="index" options={{ headerShown: false }} />
-                <Stack.Screen
-                    name="modal"
-                    options={{ presentation: "modal", title: "Modal" }}
-                />
-            </Stack>
-            <StatusBar style="auto" />
-        </ThemeProvider>
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="modal"
+                options={{ presentation: "modal", title: "Modal" }}
+            />
+            <StatusBar style='dark' />
+        </Stack>
     )
 }
+
+export default RootLayout
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,51 @@
+import { useCallback, useRef, useState } from "react"
+import { Dimensions, RefreshControl, ScrollView, StyleSheet } from "react-native"
+import WebView from "react-native-webview"
+import { ShouldStartLoadRequest } from "react-native-webview/lib/WebViewTypes"
+
 const HomeScreen = () => {
+    const webViewRef = useRef<WebView>(null)
+    const [refreshing, setRefreshing] = useState(false)
+    const onRefresh = useCallback(() => {
+        setRefreshing(true)
+        webViewRef.current?.reload()
+    }, [])
+
+    const handleShouldStartLoad = (event: ShouldStartLoadRequest) => {
+        const { url } = event
+        if (
+            url.startsWith("https://inventory-web-app-beige.vercel.app/") ||
+            url.startsWith("https://accounts.google.com/v3/signin/")
+        ) {
+            return true
+        }
+
+        return false
+    }
     return (
-        <div>
-            <h1>Welcome to the Inventory Mobile App</h1>
-        </div>
+        <ScrollView
+            style={styles.container}
+            refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+        >
+            <WebView
+                ref={webViewRef}
+                source={{ uri: "https://inventory-web-app-beige.vercel.app/" }}
+                style={styles.container}
+                onShouldStartLoadWithRequest={(e) => handleShouldStartLoad(e)}
+                onLoadEnd={() => setRefreshing(false)}
+            />
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: Dimensions.get("window").height,
+        width: Dimensions.get("window").width,
+    },
+})
 
 export default HomeScreen
